110e7b951b8791810063c918173c5af246cda186
Bao Nguyen <baonlq.97@gmail.com>
2023-04-03 15:13:11 +0700

    add new row item

--- lib/pages/navpages/home_page.dart
+++ lib/pages/navpages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_cubit/widgets/app_large_text.dart';
+import 'package:flutter_cubit/widgets/app_text.dart';
 
 import '../../misc/colors.dart';
 
@@ -38,20 +39,31 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
                 ],
               ),
             ),
-            SizedBox(height: 20,),
+            SizedBox(
+              height: 20,
+            ),
             AppLargeText(text: "Discover"),
-            SizedBox(height: 20,),
+            SizedBox(
+              height: 20,
+            ),
             Container(
               child: TabBar(
                 controller: _tabController,
                 labelColor: Colors.black,
                 unselectedLabelColor: Colors.grey,
                 indicatorSize: TabBarIndicatorSize.label,
-                indicator: CircleTabIndicator(color: AppColors.mainColor, radius: 4),
+                indicator:
+                    CircleTabIndicator(color: AppColors.mainColor, radius: 4),
                 tabs: [
-                  Tab(text: "Places",),
-                  Tab(text: "Inspiration",),
-                  Tab(text: "Emotions",),
+                  Tab(
+                    text: "Places",
+                  ),
+                  Tab(
+                    text: "Inspiration",
+                  ),
+                  Tab(
+                    text: "Emotions",
+                  ),
                 ],
               ),
             ),
@@ -61,9 +73,37 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
               child: TabBarView(
                 controller: _tabController,
                 children: [
-                  Text("Hi"),
+                  ListView.builder(
+                    itemCount: 3,
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        margin: const EdgeInsets.only(right: 15, top: 10),
+                        width: 200,
+                        height: 300,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Colors.white,
+                          image: const DecorationImage(
+                            image: AssetImage("img/mountain.jpeg"),
+                            fit: BoxFit.cover
+                          ),
+                        ),
+                      );
+                    },
+                  ),
                   Text("There"),
                   Text("Bye"),
+                ],
+              ),
+            ),
+            SizedBox(height: 20,),
+            Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  AppLargeText(text: "Explore more", size: 20,),
+                  AppText(text: "See all", color: AppColors.mainColor,)
                 ],
               ),
             )
@@ -92,15 +132,16 @@ class _CirclePainter extends BoxPainter {
   double radius;
 
   _CirclePainter({required this.color, required this.radius});
+
   @override
   void paint(Canvas canvas, Offset offset, ImageConfiguration configuration) {
-    Paint _paint = Paint();
-    _paint.color = color;
-    _paint.isAntiAlias = true;
+    Paint paint = Paint();
+    paint.color = color;
+    paint.isAntiAlias = true;
 
-    Offset _circleOffset = Offset(configuration.size!.width/2 - radius / 2, configuration.size!.height - radius);
+    Offset circleOffset = Offset(configuration.size!.width / 2 - radius / 2,
+        configuration.size!.height - radius);
 
-    canvas.drawCircle(offset + _circleOffset, radius, _paint);
+    canvas.drawCircle(offset + circleOffset, radius, paint);
   }
-
 }
