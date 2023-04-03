283c3f3edfe93470b8ca8dcc42cea8a1fef38583
Bao Nguyen <baonlq.97@gmail.com>
2023-04-03 17:38:48 +0700

    finish discover page

--- lib/pages/navpages/home_page.dart
+++ lib/pages/navpages/home_page.dart
@@ -12,102 +12,138 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
+  var images = {
+    "balloning.png": "Balloning",
+    "kayaking.png": "Kayaking",
+    "snorkling.png": "Snorkling",
+    "hiking.png": "Hiking"
+  };
   @override
   Widget build(BuildContext context) {
     TabController _tabController = TabController(length: 3, vsync: this);
     return Scaffold(
       body: Container(
-        margin: EdgeInsets.only(top: 40, left: 20, right: 20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              child: Row(
-                children: [
-                  Icon(
-                    Icons.menu,
-                    size: 30,
-                  ),
-                  Expanded(child: Container()),
-                  Container(
-                    width: 50,
-                    height: 50,
-                    decoration: BoxDecoration(
-                        color: Colors.grey.withOpacity(0.5),
-                        borderRadius: BorderRadius.circular(10)),
-                  )
-                ],
+        padding: EdgeInsets.only(top: 40, left: 20, right: 20),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.menu,
+                      size: 30,
+                    ),
+                    Expanded(child: Container()),
+                    Container(
+                      width: 40,
+                      height: 40,
+                      decoration: BoxDecoration(
+                          color: Colors.grey.withOpacity(0.5),
+                          borderRadius: BorderRadius.circular(10)),
+                    )
+                  ],
+                ),
               ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            AppLargeText(text: "Discover"),
-            SizedBox(
-              height: 20,
-            ),
-            Container(
-              child: TabBar(
-                controller: _tabController,
-                labelColor: Colors.black,
-                unselectedLabelColor: Colors.grey,
-                indicatorSize: TabBarIndicatorSize.label,
-                indicator:
-                    CircleTabIndicator(color: AppColors.mainColor, radius: 4),
-                tabs: [
-                  Tab(
-                    text: "Places",
-                  ),
-                  Tab(
-                    text: "Inspiration",
-                  ),
-                  Tab(
-                    text: "Emotions",
-                  ),
-                ],
+              SizedBox(height: 10,),
+              AppLargeText(text: "Discover"),
+              SizedBox(height: 5,),
+              Container(
+                child: TabBar(
+                  controller: _tabController,
+                  labelColor: Colors.black,
+                  unselectedLabelColor: Colors.grey,
+                  indicatorSize: TabBarIndicatorSize.label,
+                  indicator:
+                      CircleTabIndicator(color: AppColors.mainColor, radius: 4),
+                  tabs: [
+                    Tab(
+                      text: "Places",
+                    ),
+                    Tab(
+                      text: "Inspiration",
+                    ),
+                    Tab(
+                      text: "Emotions",
+                    ),
+                  ],
+                ),
               ),
-            ),
-            Container(
-              width: double.maxFinite,
-              height: 300,
-              child: TabBarView(
-                controller: _tabController,
-                children: [
-                  ListView.builder(
-                    itemCount: 3,
-                    scrollDirection: Axis.horizontal,
-                    itemBuilder: (context, index) {
-                      return Container(
-                        margin: const EdgeInsets.only(right: 15, top: 10),
-                        width: 200,
-                        height: 300,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          color: Colors.white,
-                          image: const DecorationImage(
-                            image: AssetImage("img/mountain.jpeg"),
-                            fit: BoxFit.cover
+              Container(
+                width: double.maxFinite,
+                height: 230,
+                child: TabBarView(
+                  controller: _tabController,
+                  children: [
+                    ListView.builder(
+                      itemCount: 3,
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (context, index) {
+                        return Container(
+                          margin: const EdgeInsets.only(right: 15, top: 10),
+                          width: 150,
+                          height: 230,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: Colors.white,
+                            image: const DecorationImage(
+                              image: AssetImage("img/mountain.jpeg"),
+                              fit: BoxFit.cover
+                            ),
                           ),
+                        );
+                      },
+                    ),
+                    Text("There"),
+                    Text("Bye"),
+                  ],
+                ),
+              ),
+              SizedBox(height: 5,),
+              Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    AppLargeText(text: "Explore more", size: 20,),
+                    AppText(text: "See all", color: AppColors.mainColor,)
+                  ],
+                ),
+              ),
+              SizedBox(height: 10,),
+              Container(
+                height: 100,
+                width: double.maxFinite,
+                child: ListView.builder(
+                  itemCount: 4,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index) {
+                  return Container(
+                    margin: const EdgeInsets.only(right: 20),
+                    child: Column(
+                      children: [
+                        Container(
+                          width: 60,
+                          height: 60,
+                          decoration: BoxDecoration(
+                              color: Colors.grey.withOpacity(0.5),
+                              borderRadius: BorderRadius.circular(10),
+                          image: DecorationImage(
+                              image: AssetImage("img/${images.keys.elementAt(index)}"),
+                              fit: BoxFit.cover
+                          ))
                         ),
-                      );
-                    },
-                  ),
-                  Text("There"),
-                  Text("Bye"),
-                ],
-              ),
-            ),
-            SizedBox(height: 20,),
-            Container(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  AppLargeText(text: "Explore more", size: 20,),
-                  AppText(text: "See all", color: AppColors.mainColor,)
-                ],
-              ),
-            )
-          ],
+                        SizedBox(height: 10,),
+                        Container(
+                          child: AppText(text: "${images.values.elementAt(index)}",),
+                        )
+                      ],
+                    ),
+                  );
+                },)
+              )
+            ],
+          ),
         ),
       ),
     );
